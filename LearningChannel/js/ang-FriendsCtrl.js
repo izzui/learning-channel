af2c89ae567eb695f57d6b412348a81ac6cc5d94
vicentesarmento <vicente.sarmento@izzui.com>
2013-08-20 19:16:39 -0300

    finalizacao do curso inscrito

---
+++ LearningChannel/js/ang-FriendsCtrl.js
@@ -0,0 +1,15 @@
+﻿//Leaderboard
+function FriendsCtrl($scope) {
+	$scope.friends = [
+		{ picture: "pic-ID00001.jpg", name: "Chuck", lastname: "Norris"},
+		{ picture: "pic-ID00002.jpg", name: "Ryan", lastname: "Tester"},
+		{ picture: "pic-ID00003.jpg", name: "Jon", lastname: "Jones"},
+		{ picture: "pic-ID00004.jpg", name: "Chuck", lastname: "Lidel"},
+		{ picture: "pic-ID00005.jpg", name: "Hulk", lastname: "Hogan"},
+		{ picture: "pic-ID00006.jpg", name: "Rocky", lastname: "Balboa"},
+		{ picture: "pic-ID00007.jpg", name: "Liu", lastname: "Kang"},
+		{ picture: "pic-ID00008.jpg", name: "Bart", lastname: "Simpson"},
+		{ picture: "pic-ID00009.jpg", name: "Fred", lastname: "Flintstone"},
+		{ picture: "pic-ID00010.jpg", name: "Marty", lastname: "McFly"},
+	];
+}
