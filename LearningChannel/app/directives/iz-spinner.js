0f62c2d567d52cc3c0d763a512f6e30f811024f9
demetriusnunes <demetriusnunes@gmail.com>
2013-08-23 15:15:02 -0300

    Adicionando spinner para chamadas Ajax

---
+++ LearningChannel/app/directives/iz-spinner.js
@@ -0,0 +1,33 @@
+﻿izzuiApp
+	.directive('izSpinner', ['$window', function ($window) {
+	    return {
+	        scope: true,
+	        link: function (scope, element, attr) {
+	            function createSpinner(options) {
+	                if (scope.spinner) {
+	                    scope.spinner.stop();
+	                }
+	                scope.spinner = new $window.Spinner(scope.options);
+	                scope.spinner.spin(element[0]);
+	            }
+
+	            scope.spinner = null;
+
+	            scope.$watch(attr.izSpinner, function (options) {
+	                scope.options = options;
+	                createSpinner();
+	            }, true);
+
+	            scope.$watch(attr.hideSpinner, function (val) {
+	                if (!val) {
+	                    createSpinner();
+	                }
+	                else {
+                        if (scope.spinner)
+                            scope.spinner.stop();
+                    }
+	            }, true);
+
+	        }
+	    };
+	}]);
