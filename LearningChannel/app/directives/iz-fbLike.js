52bc8799fc726f58654ed95e012a473dfc563161
andreybritto <andrey.britto@izzui.com>
2013-08-23 14:36:16 -0300

    VideoPlayer final, com navegação e like funcionando

---
+++ LearningChannel/app/directives/iz-fbLike.js
@@ -0,0 +1,14 @@
+﻿angular.module('izzuiApp').directive("fbLike", function () {
+	return {
+		restrict: 'A',
+		link: function (scope, element, attrs) {
+			attrs.$observe('href', function (value) {
+				//attrs.id = "fbLike" + new Date().getTime();
+				//element.attr('id', attrs.id);
+				if (attrs.href.indexOf('/', attrs.href.length - 1) === -1) {
+					return typeof FB !== "undefined" && FB !== null ? FB.XFBML.parse(element.parent()[0]) : void 0;
+				}
+			});
+		}
+	};
+});
