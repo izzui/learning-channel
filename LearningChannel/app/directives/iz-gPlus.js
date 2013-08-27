96539872232ac859a3ab3ee95605fa253bda5200
andreybritto <andrey.britto@izzui.com>
2013-08-27 14:26:36 -0300

    Mais uma tentativa do GPlus

--- LearningChannel/app/directives/iz-gPlus.js
+++ LearningChannel/app/directives/iz-gPlus.js
@@ -3,15 +3,17 @@
 		restrict: 'A',
 		link: function (scope, element, attrs) {
 			console.log('gPlus directive');
-			//attrs.$observe('href', function (value) {
+			attrs.$observe('href', function (value) {
 				console.log('opa gplus');
+				console.log('|' + attrs.href + '|');
 				if (attrs.href.indexOf('/', attrs.href.length - 1) === -1) {
+				
 					console.log('arrego gplus');
 					attrs.dataHref = attrs.href;
 					element.attr('data-href', attrs.dataHref);
 					gapi.plusone.go();
 				}
-			//});
+			});
 		}
 	};
 });
