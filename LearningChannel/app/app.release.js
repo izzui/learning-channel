99c95a0896c0f49d9fc548dc332b88acf0a90a05
andreybritto <andrey.britto@izzui.com>
2013-09-24 10:10:11 -0300

    Consertando o App.Js para o Release

--- LearningChannel/app/app.release.js
+++ LearningChannel/app/app.release.js
@@ -1,15 +1,15 @@
 ﻿var izzuiApp = angular.module('izzuiApp', ['ngCookies'])
-  .config(function ($routeProvider, $httpProvider) {
-      $routeProvider.
-          when('/course/:courseId', { templateUrl: 'app/views/course.html' }).
-          when('/', { templateUrl: 'app/views/home.html' }).
-          when('/course', { templateUrl: 'app/views/course.html' }).
-          when('/groups', { templateUrl: 'app/views/groups.html' }).
-          when('/course-unsub/:courseId', { templateUrl: 'app/views/course-unsub.html' }).
-	      when('/video/:videoId', { templateUrl: 'app/views/video.html' }).
-          otherwise({ redirectTo: '/' });
-
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	.config(function ($routeProvider, $httpProvider, $locationProvider) {
+		$locationProvider.html5Mode(false).hashPrefix('!');
+		$routeProvider.
+			when('/course/:courseId', { templateUrl: 'app/views/course.html' }).
+			when('/', { templateUrl: 'app/views/home.html' }).
+			when('/course', { templateUrl: 'app/views/course.html' }).
+			when('/groups', { templateUrl: 'app/views/groups.html' }).
+			when('/course-unsub/:courseId', { templateUrl: 'app/views/course-unsub.html' }).
+			when('/video/:videoId', { templateUrl: 'app/views/video.html' }).
+			otherwise({ redirectTo: '/' });
+		delete $httpProvider.defaults.headers.common['X-Requested-With'];
   })
   .run(function ($rootScope, $location) {
       $rootScope.$on('$includeContentLoaded', initEnquire);
