7dc27116ae8249e9f1ee494a5cf3866e0616ce63
andreybritto <andrey.britto@izzui.com>
2013-10-17 15:23:55 -0300

    alterando para colocar no ar

--- LearningChannel/app/app.js
+++ LearningChannel/app/app.js
@@ -1,7 +1,7 @@
 ﻿var izzuiApp = angular.module('izzuiApp', ['ngCookies'])
 	.config(function ($routeProvider, $httpProvider, $locationProvider) {
 		$locationProvider.html5Mode(false).hashPrefix('!');
-  		$routeProvider.
+		$routeProvider.
 			when('/course/:courseId', { templateUrl: 'app/views/course.html' }).
 			when('/', { templateUrl: 'app/views/home.html' }).
 			when('/course', { templateUrl: 'app/views/course.html' }).
@@ -9,13 +9,12 @@
 			when('/course-unsub/:courseId', { templateUrl: 'app/views/course-unsub.html' }).
 			when('/video/:videoId', { templateUrl: 'app/views/video.html' }).
 			otherwise({ redirectTo: '/' });
-
 		delete $httpProvider.defaults.headers.common['X-Requested-With'];
-	})
-	.run(function ($rootScope, $location) {
-		$rootScope.$on('$includeContentLoaded', initEnquire);
-		$rootScope.$on('$viewContentLoaded', initEnquire);
-		$rootScope.baseApiUrl = $location.protocol() + "://local-learningchannelapi.izzui.com/";
-		$rootScope.facebookAppId = '212553442202383';
-		$rootScope.appAccessToken = '212553442202383|zIGD114FUd3RihFP8ENA14NlsZE';
-	});
+  })
+  .run(function ($rootScope, $location) {
+      $rootScope.$on('$includeContentLoaded', initEnquire);
+      $rootScope.$on('$viewContentLoaded', initEnquire);
+      $rootScope.baseApiUrl = 'http://lc-tiffanyapi.elasticbeanstalk.com/';
+      $rootScope.facebookAppId = '1387197538175604';
+      $rootScope.appAccessToken = '1387197538175604|N0axbhqocroVn7RySF-qTiDikPQ';
+  });
